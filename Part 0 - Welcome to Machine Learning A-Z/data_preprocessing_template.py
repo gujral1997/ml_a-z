4e7a89baf6cc650dc9f7dda636d041ec10157091
a <a>
2018-04-22 15:27:22 -0400

    linear regression dataset

--- Part 0 - Welcome to Machine Learning A-Z/data_preprocessing_template.py
+++ Part 0 - Welcome to Machine Learning A-Z/data_preprocessing_template.py
@@ -15,19 +15,6 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'median', axis = 0) # 0 for along columns and 1 for along rows
 imputer.fit(X[:,1:3]) # 3 is exclude (1 and 2 have missing data)
 X[:, 1:3] = imputer.transform(X[:, 1:3]) # missing data will be filled
-print (X)
-
-# Splitting the dataset into the Training set and Test set
-# from sklearn.cross_validation import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
 # Encoding categorical data (countries here)
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
