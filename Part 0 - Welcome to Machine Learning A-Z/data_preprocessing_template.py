10af50dcdaf7a04f52d0fce56a1aa600979db175
a <a>
2018-04-22 08:49:50 -0400

    added missing data

--- Part 0 - Welcome to Machine Learning A-Z/data_preprocessing_template.py
+++ Part 0 - Welcome to Machine Learning A-Z/data_preprocessing_template.py
@@ -10,9 +10,16 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+# filling the missing values
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'median', axis = 0) # 0 for along columns and 1 for along rows
+imputer.fit(X[:,1:3]) # 3 is exclude (1 and 2 have missing data)
+X[:, 1:3] = imputer.transform(X[:, 1:3]) # missing data will be filled
+print (X)
+
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# from sklearn.cross_validation import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
